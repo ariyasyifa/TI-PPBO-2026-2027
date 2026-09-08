73671175e41e4f0bea1ea2abbd70c88e58061062
ariya ssyifa <ariyasyifa76@gmail.com>
2026-09-08 09:31:31 +0700

    Deklarasi Variabel dan Tipe Data Primitif

---
+++ parktikum-2/src/VariabelDemo.java
@@ -0,0 +1,16 @@
+public class VariabelDemo {
+    public static void main(String[] args) {
+        String nama = "ariya ssyifa";
+        int umur = 19;
+        double tinggi = 165;
+        char golDarah = 'b';
+        boolean mahasiswaaktif = true;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Golongan Darah: " + golDarah);
+        System.out.println("Aktif:" + mahasiswaaktif);
+
+    }
+}
