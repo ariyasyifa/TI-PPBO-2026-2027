5122c1a4cdd9c76138c1b6977ca675329b0321ec
ariya ssyifa <ariyasyifa76@gmail.com>
2026-09-08 08:59:17 +0700

    Struktur Dasar Program Java

--- parktikum-2/src/HelloWorld.java
+++ parktikum-2/src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("halo, ariya ssyifa");
+        System.out.print("halo, ariya ssyifa");
+        System.out.print("nama: ");
+        System.out.print("ariya ssyifa");
+        System.out.println();
+        System.out.println("ini baris baru");
     }
 }
