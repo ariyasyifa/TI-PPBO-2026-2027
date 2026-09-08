e78df0135f77c613c9bcf787e44d0d8913f5efad
ariya ssyifa <ariyasyifa76@gmail.com>
2026-09-08 11:15:16 +0700

    Cara Membuat Biodata

---
+++ parktikum-2/src/BiodataSaya.java
@@ -0,0 +1,9 @@
+public class BiodataSaya {
+    public static void main (String[] args) {
+        //menampilkan biodata sederhana
+        System.out.println("Nama : Ariya ssyifa");
+        System.out.println("NIM.2025573010119");
+        System.out.println("Program studi : Teknik Informatika");
+        //menampilkan atau mencetak nama program studi
+    }
+}
