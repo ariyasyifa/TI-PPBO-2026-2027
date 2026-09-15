e5a141c6d0f4d502bc184bcadaab807746398e00
ariya ssyifa <ariyasyifa76@gmail.com>
2026-09-15 17:41:38 +0700

    If Bertingkat(else if)

--- parktikum-2/src/IfDemo.java
+++ parktikum-2/src/IfDemo.java
@@ -1,12 +1,17 @@
 public class IfDemo {
     public static void main(String[] args) {
+        int nilai = 78;
+        char huruf;
 
-        int nilai = 60;
-
-        if(nilai >= 75) {
-            System.out.println("Anda LULUS");
+        if(nilai >= 85) {
+            huruf = 'A';
+        } else if (nilai >= 70) {
+            huruf = 'B';
+        } else if (nilai >= 55) {
+            huruf = 'C';
         } else {
-            System.out.println("Anda TIDAK LULUS");
+            huruf = 'E';
         }
+        System.out.println("Nilai huruf: " + huruf);
     }
 }
