91aac768f24f91dce55b0362ad37422e220694c5
ariya ssyifa <ariyasyifa76@gmail.com>
2026-09-17 15:34:28 +0700

    Menentukan bilangan terbesar

---
+++ parktikum-2/src/LatihanBilangan.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class LatihanBilangan {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukan bilangan pertama: ");
+        int a = input.nextInt();
+
+        System.out.print("Masukan bilangan kedua: ");
+        int b = input.nextInt();
+
+        System.out.print("Masukan bilangan ketiga: ");
+        int c = input.nextInt();
+
+        if (a >= b && a >= c) {
+            System.out.println("Bilangan terbesar: " + a);
+        }else if (b >= a && b >= c) {
+            System.out.println("Bilangan terbesar: " + b);
+        }else {
+            System.out.println("Bilangan terbesar: " + c);
+        }
+    }
+}
